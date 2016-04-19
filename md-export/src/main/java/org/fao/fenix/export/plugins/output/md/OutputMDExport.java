b4d79e3f5ccb3a651c0edcb99fb66d4041d59852
fabriziofenix <fabrizio.castelli@fao.org>
2016-04-19 18:51:48 +0200

    ..

--- md-export/src/main/java/org/fao/fenix/export/plugins/output/md/OutputMDExport.java
+++ md-export/src/main/java/org/fao/fenix/export/plugins/output/md/OutputMDExport.java
@@ -33,7 +33,7 @@ public class OutputMDExport extends Output {
     private Map<String, Object> config;
     private MeIdentification metadata;
     private DataCreator dataCreator ;
-    private final String MDSD_URL = "http://localhost:7777/v2/mdsd";
+    private final String MDSD_URL = "http://fenix.fao.org/d3s/mdsd";
     private JsonNode mdsdNode;
     private ByteArrayOutputStream baos;
 
